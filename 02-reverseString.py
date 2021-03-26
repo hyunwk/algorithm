bc708f83d4012aca57a26d57e9906e04cee10a66
ab8454 <ab8454@naver.com>
2021-03-26 17:50:46 +0900

    feat: 02

---
+++ 02-reverseString.py
@@ -0,0 +1,23 @@
+from typing import List
+class foo:
+    def reverseString1(self, s: List[str]) -> None:
+        left, right = 0, len(s) -1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+    def reverseString2(self, s: List[str]) -> None:
+        s.reverse()
+s =  ["s", "d"]
+f = foo()
+
+for i in s:
+    print(i, end=' ')
+f.reverseString1(s)
+for i in s:
+    print(i, end=' ')
+print()
+f.reverseString2(s)
+for i in s:
+    print(i, end=' ')
