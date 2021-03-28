8f9e40560525011ab576264763c4289a669829f0
ab8454 <ab8454@naver.com>
2021-03-28 20:55:59 +0900

    feat: 10

---
+++ 10-arrayPairSum.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def arrayPairSum1(self, nums: List[int]) -> int:
+        sum = 0
+        pair = []
+        nums.sort()
+
+        for n in nums:
+            pair.append(n)
+            if len(pair) == 2:
+                sum += min(pair)
+                pair = []
+        return sum
+    def arrayPairSum2(self, nums: List[int])->int:
+        sum = 0
+
+        for i, n in enumerate(nums):
+            if i % 2 == 0:
+                sum += n
+        return sum
+
+    def arrayPairSum3(self, nums: List[int])->int:
+        return sum(sorted(nums)[::2])
+nums = [6,2,6,5,1,2]
+b = Solution()
+print(b.arrayPairSum1(nums))
+print(b.arrayPairSum2(nums))
+print(b.arrayPairSum3(nums))
