3507fac3251c1fd2ff4f5e2ec7b1c3791802b1e6
hyunwk <ab8454@naver.com>
2022-03-21 22:00:21 +0900

    ✨ https://leetcode.com/explore/interview/card/top-interview-questions-easy/99/others/601/

--- leetcode/easy/PascalsTriangle.js
+++ leetcode/easy/PascalsTriangle.js
@@ -3,13 +3,20 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
+    // let result = [[1]];
+    // for (let i=1 ; i<numRows ; i++) {
+    //     result.push([])
+    //     result[i][0] = result[i][i] = 1;
+    //     for (let j=1 ; j<i ; j++)
+    //         result[i][j] = result[i-1][j-1] + result[i-1][j];
+    // }
+    // return result;
     let result = [[1]];
     for (let i=1 ; i<numRows ; i++) {
-        result.push([])
-        result[i][0] = result[i][i] = 1;
-        for (let j=1 ; j<i ; j++) {
-            result[i][j] = result[i-1][j-1] + result[i-1][j];
-        }
+        left = [...result[i-1], 0];
+        right = [0, ...result[i-1]];
+        let newRow = left.map((item, index) => item + right[index]);
+        result.push(newRow);
     }
     return result;
 };
