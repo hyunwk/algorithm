aeaadc32e90f678e777e42d6226599b845eeb081
hyunwk <ab8454@naver.com>
2022-04-05 00:45:10 +0900

    ✨ https://leetcode.com/problems/swap-nodes-in-pairs/submissions/

---
+++ leetcode/easy/SwapNodesInPairs.js
@@ -0,0 +1,10 @@
+var swapPairs = function(head) {
+    if (!head || !head.next)
+        return head;
+    
+    let temp = head.next;
+    head.next = swapPairs(head.next.next);
+    temp.next = head;
+    
+    return temp;
+};
